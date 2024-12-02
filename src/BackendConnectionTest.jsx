71fa04dd2735cb3ea3fd55c859cc7590cc8cdfa2
norbertc5 <norbert07c@gmail.com>
2024-12-02 22:38:13 +0100

    Deleted ugly stuff made by Wojtek

---
+++ src/BackendConnectionTest.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+function BackendConnectionTest()
+{
+    useEffect(()=>{
+       // fetch("localhost:8080/api/v1")
+    }, [])
+
+    return(<>
+
+    </>);
+}
+export default BackendConnectionTest;

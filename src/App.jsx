a2519340abac53923d2d38ddfc35a6d9954dbac4
Wojtekgithub <w.maciaszek.tp22@zsme.tarnow.pl>
2024-12-07 22:11:27 +0100

    It is getting better

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Menu, Login, Registration } from "./form";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [showFormToLogIn, setShowFormToLogIn] = useState(false);
+  const [showCreateAccount, setshowCreateAccount] = useState(false);
+
+  // let showFormToLogIn = false;
+  function buttonLogIn() {
+    setShowFormToLogIn(!showFormToLogIn);
+    setshowCreateAccount(false);
+  }
+  function functionShowCreateAccount() {
+    setshowCreateAccount(!showCreateAccount);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>To jest strona react + vite</h1>
-      <p>A tu zrobimy aplikacje :)</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Menu buttonLogIn={buttonLogIn}></Menu>
 
-export default App
+      {showFormToLogIn && (
+        <div id="formField">
+          <Login functionShowCreateAccount={functionShowCreateAccount}></Login>
+        </div>
+      )}
+      {showCreateAccount && (
+        <div id="formField">
+          <Registration></Registration>
+        </div>
+      )}
+    </>
+  );
+}

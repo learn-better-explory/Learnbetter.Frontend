8fbb11288149a3df375a29313f9dd795c4f6bda8
Wojtekgithub <w.maciaszek.tp22@zsme.tarnow.pl>
2024-12-12 20:56:35 +0100

    something

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,8 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/logowanie" element={<Login />} />
-      <Route path="/rejestracja" element={<Registration />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
     </Routes>
   </BrowserRouter>
 );
